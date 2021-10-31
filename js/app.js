8622ed098b7bfbb81ec746d21e47ed2d3d34c615
Ismayil Mirzayev <ismayil.mirzayev0@gmail.com>
2021-11-01 03:34:21 +0400

    Sidebar collapse

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 $(() => {
   $("#sidebar").load("components/sidebar.html");
+  $("#header").load("components/header.html");
+  $("#footer").load("components/footer.html");
+
+  $(document).on("keyup", (e) => {
+    $(".sidebar").toggleClass("sidebar--collapsed");
+  });
 });
