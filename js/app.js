6cb3f90a077826516e050380aa84b224ff1d3e5a
Ismayil Mirzayev <ismayil.mirzayev0@gmail.com>
2021-11-01 14:06:52 +0400

    Created sidebar dropdown

--- js/app.js
+++ js/app.js
@@ -6,4 +6,21 @@ $(() => {
   $(document).on("keyup", (e) => {
     $(".sidebar").toggleClass("sidebar--collapsed");
   });
+
+  $(document).on("click", ".sidebar__link", function (e) {
+    e.preventDefault();
+    $(".sidebar").removeClass("sidebar--collapsed");
+    $(".sidebar__link").not(this).removeClass("sidebar__link--active");
+    $(this).toggleClass("sidebar__link--active");
+    $(".sidebar__dropdown")
+      .not($(this).siblings(".sidebar__dropdown"))
+      .slideUp();
+    $(this).siblings(".sidebar__dropdown").slideToggle();
+  });
+
+  $(document).on("click", ".sidebar__toggle", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("sidebar__toggle--active");
+    $(".sidebar").toggleClass("sidebar--collapsed");
+  });
 });
